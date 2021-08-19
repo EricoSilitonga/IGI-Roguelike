8f360e11d66e067ecf89166180af18153375469e
Rizqihafidz <aractorz123@gmail.com>
2021-08-19 12:50:56 +0700

    Add room prefab and RoomSpawner script

---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 = need B door
+    // 2 = need T door
+    // 3 = need L door
+    // 4 = need R door
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(openingDirection == 1)
+        {
+
+        }else if (openingDirection == 2)
+        {
+
+        }else if (openingDirection == 3)
+        {
+
+        }else if (openingDirection == 4)
+        {
+
+        }
+    }
+}
