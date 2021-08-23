76e4d0d83e624bb1a6e6b44ac3fee2d59bca4e51
EricoSilitonga <ericosilitonga@yahoo.com>
2021-08-23 17:49:58 +0700

    Revert "Add BGM_Menu"
    This reverts commit 64ad2738f32b455e1674d193bd8755bd15e44240.

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -18,7 +18,7 @@ public class RoomSpawner : MonoBehaviour
 
     private void Start()
     {
-        //Destroy(gameObject, waitTime);
+        Destroy(gameObject, waitTime);
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         Invoke("Spawn", 0.1f);
     }
