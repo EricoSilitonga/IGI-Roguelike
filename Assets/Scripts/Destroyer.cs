05656bf0770adf423d9b55183f685a73b21ffdba
Rizqihafidz <aractorz123@gmail.com>
2021-08-25 09:04:51 +0700

    Edit enrtry room prefab

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -11,7 +11,11 @@ public class Destroyer : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("ClosedRoom"))
+        if (collision.CompareTag("Player"))
+        {
+            
+        }
+        else
         {
             Destroy(collision.gameObject);
         }
